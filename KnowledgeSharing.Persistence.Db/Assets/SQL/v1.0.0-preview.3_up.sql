d115e36864e4440d4b3b0741144e0d14dd8ffae9
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-05-22 17:13:17 +0200

    fix: wrong name for a column in order table

---
+++ KnowledgeSharing.Persistence.Db/Assets/SQL/v1.0.0-preview.3_up.sql
@@ -0,0 +1,19 @@
+﻿START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220522151153_correct-column-name') THEN
+    ALTER TABLE store."order" RENAME COLUMN "FinalizedAt" TO finalized_at;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220522151153_correct-column-name') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20220522151153_correct-column-name', '6.0.4');
+    END IF;
+END $EF$;
+COMMIT;
+
---
+++ KnowledgeSharing.Persistence.Db/Migrations/20220522151153_correct-column-name.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KnowledgeSharing.Persistence.EFCoreFluentAPI.Migrations
+{
+    public partial class correctcolumnname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinalizedAt",
+                schema: "store",
+                table: "order",
+                newName: "finalized_at");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "finalized_at",
+                schema: "store",
+                table: "order",
+                newName: "FinalizedAt");
+        }
+    }
+}
